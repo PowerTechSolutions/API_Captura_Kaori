36092b190e14e43076898d93b6fb23c7d85c2f7e
kaori_katayama <125297000+Kaori2@users.noreply.github.com>
2023-11-29 14:20:04 -0300

    Add files via upload

---
+++ indv_kaori/src/main/kotlin/Conexao.kt
@@ -0,0 +1,33 @@
+
+import org.apache.commons.dbcp2.BasicDataSource
+import org.springframework.jdbc.core.JdbcTemplate
+
+object ConexaoMySql {
+
+        var jdbcTemplate: JdbcTemplate? = null
+            get() {
+                if (field == null){
+
+                    val dataSource = BasicDataSource()
+
+                    dataSource.driverClassName = "com.mysql.cj.jdbc.Driver"
+                    dataSource.url = "jdbc:mysql://localhost:3306/PowerTechSolutions"
+                    dataSource.username = "root"
+                    dataSource.password = "@Icecubes123"
+
+                    val novojdbcTmeplate = JdbcTemplate(dataSource)
+
+                    field = novojdbcTmeplate
+
+                }
+
+                return field
+
+            }
+
+    fun iniciarSql(){
+        jdbcTemplate = jdbcTemplate!!
+    }
+
+}
+
