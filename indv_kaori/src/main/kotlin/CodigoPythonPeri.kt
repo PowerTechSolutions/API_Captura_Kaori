6fe2b30c1460ccaf691aa88c58d76fa7f1381b77
kaori_katayama <michelly.katayama@sptech.school>
2023-11-30 17:26:41 -0300

    DENOVO

--- indv_kaori/src/main/kotlin/CodigoPythonPeri.kt
+++ indv_kaori/src/main/kotlin/CodigoPythonPeri.kt
@@ -21,7 +21,9 @@ object CodigoPythonPeri {
         }
 
         var codigoPython ="""
+import time
 import psutil
+import mysql.connector
 import pymssql
 
 disco = psutil.disk_usage('/')
@@ -38,20 +40,35 @@ try:
 
     cursor_mssql.execute(
         "INSERT INTO Monitoramento_RAW (Total, Free, Uso, Porcentagem, FKComponente_Monitorado) VALUES (%s, %s, %s, %s, %s)",
-        (disco.total, disco.free, disco.used, disco.percent, ${componenteDISCO})  # Replace 1 with the actual value for FKComponente_Monitorado
+        (disco.total, disco.free, disco.used, disco.percent, $componenteDISCO)  # Replace 1 with the actual value for FKComponente_Monitorado
     )
     conn_mssql.commit()
 
+    # MySQL Connection
+    mydb = mysql.connector.connect(
+        host='localhost',
+        user='root',
+        password='@Icecubes123',
+        database='PowerTechSolutions'
+    )
+    mycursor = mydb.cursor()
+
+    if mydb.is_connected():
+        sql_querryDISCO = 'INSERT INTO Monitoramento_RAW (Total, Free, Uso, Porcentagem, FKComponente_Monitorado) VALUES ( %s, %s, %s, %s, %s)'
+        valDISCO = [disco.total, disco.used, disco.free, disco.percent, $componenteDISCO]  # Replace 1 with the actual value for FKComponente_Monitorado
+        mycursor.execute(sql_querryDISCO, valDISCO)
+        mydb.commit()
+
 finally:
-    # MSSQL Cleanup
-    cursor_mssql.close()
-    conn_mssql.close()
+    if mydb.is_connected():
+        mycursor.close()
+        mydb.close()
 """
 
         val nomeArquivoPyDefault = "CodigoPythonPeriKaori.py"
 
         File(nomeArquivoPyDefault).writeText(codigoPython)
-        Runtime.getRuntime().exec("py $nomeArquivoPyDefault")
+        Runtime.getRuntime().exec("python3 $nomeArquivoPyDefault")
 
         println("Disco capturado")
     }
